140e8d2b362d47e4abb0123fdb4a2bd9a9406be9
roland <roland.erkiert@gmail.com>
2021-01-23 11:23:48 +0100

    First test with pytest framework

--- calculator/CalculatorPage.py
+++ calculator/CalculatorPage.py
@@ -1,5 +1,12 @@
+from selenium.webdriver.common.by import By
+
 from utils.BasePage import BasePage
+from utils.BaseElement import BaseElement
 
 
 class CalculatorPage(BasePage):
-    pass
+    @property
+    def legend(self):
+        locator = (By.CSS_SELECTOR, 'legend')
+        return BaseElement(self.driver, locator)
+
--- test_suite.py
+++ test_suite.py
@@ -1,13 +1,18 @@
-import time
 from selenium import webdriver
-
 from calculator.CalculatorPage import CalculatorPage
 
 driver = webdriver.Chrome()
 url_calculator = 'https://rololand.github.io/app-for-tests'
 calculator = CalculatorPage(driver=driver, url=url_calculator)
 
-calculator.go()
 
-time.sleep(3)
-calculator.quit()
+def setup_function():
+    calculator.go()
+
+
+def teardown_function():
+    calculator.quit()
+
+
+def test_check_legend_name():
+    assert calculator.legend.text == 'Kalkulator:'
